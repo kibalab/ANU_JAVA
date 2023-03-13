503b7b2d38485b08087eb0513b024ea010f13ff2
KIBA <kjh030529@gmail.com>
2023-03-13 16:40:31 +0900

    Commit Test02 : Calculate circle radius

---
+++ Test02/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        final double PI = 3.141592; // 상수 선언
+        double radius, area;
+
+        radius = 5.0;
+        area = PI * radius * radius;
+        System.out.println("반지름이 5인 원의 면적은 : " + area);
+    }
+}
